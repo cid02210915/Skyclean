d973974fc0b79d60912d8bcc84d140b367453312
CodefromMax <max0829@126.com>
2024-06-04 17:12:14 -0400

    Update CMB_data Class.

--- skyclean/CMB_data/test.py
+++ skyclean/CMB_data/test.py
@@ -3,3 +3,16 @@ from cmb_data import CMB_Data
 map1 = CMB_Data("planck_simulation/ffp10_newdust_total_030_full_map.fits")
 
 print(map1.nside)
+
+# Skyclean 
+
+# Skyfilter
+
+# Byedust
+
+# Dustfilter
+
+# Seebetter
+
+# Cbetter
+
--- skyclean/tests/read_map.py
+++ skyclean/tests/read_map.py
@@ -1,5 +1,5 @@
-from skyclean import CMB_Data
+# from skyclean import CMB_Data
 
-map1 = CMB_Data("planck_simulation/ffp10_newdust_total_030_full_map.fits")
+# map1 = CMB_Data("planck_simulation/ffp10_newdust_total_030_full_map.fits")
 
-print(map1.nside())
+# print(map1.nside())
