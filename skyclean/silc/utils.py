5650954660ba86ca412ccecec669bd4019da7201
cid02210915 <al2522@ic.ac.uk>
2025-09-14 15:35:35 -0400

    +_norm

--- skyclean/silc/utils.py
+++ skyclean/silc/utils.py
@@ -79,3 +79,8 @@ def save_array(task):
     os.makedirs(os.path.dirname(out_path), exist_ok=True)
     np.save(out_path, arr)
     return out_path
+
+@staticmethod
+def _norm(v: np.ndarray) -> np.ndarray:
+    n = np.linalg.norm(v)
+    return v if n == 0 else (v / n)
